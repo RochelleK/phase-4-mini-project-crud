d7a9f0ac1764337407c8125e68287d1beb1f3bf0
Rochelle Katzman <zelrochelle@gmail.com>
2023-01-31 17:42:42 -0500

    commit tree b108637e4f5a9b81c89aaed15aef1865ce2cabb6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :spices, only: [:index, :create, :update, :destroy]
+
+  # the below are commented out but do the same thing as the above 
+  # get '/spices', to: 'spices#index'
+  
+  # post '/spices', to: 'spices#create'
+  
+  # patch '/spices/:id', to: 'spices#update'
+  
+  # delete '/spices/:id', to: 'spices#destroy'
+
+
 end
